7f476b75aa2f2764b64536977c1dbce2dfbe1a95
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-22 16:47:39 -0400

    created a createSuggestino controller to accept input for new suggestions

--- routes/suggestion/controller/suggestionController.js
+++ routes/suggestion/controller/suggestionController.js
@@ -9,5 +9,22 @@ module.exports = {
 				callback(null, foundSuggestion);
 			}
 		});
-	}
+	},
+    createSuggestion: (body, callback) => {
+		const createdSuggestion = new Suggestion({
+			title: body.title.toLowerCase(),
+			author: body.author.toLowerCase(),
+			suggestion: body.suggestion.toLowerCase(),
+            likes: body.likes,
+            anonymous: body.anonymous
+		});
+
+		createdSuggestion.save(function (err, savedSuggestion) {
+			if (err) {
+				callback(err, null);
+			} else {
+				callback(null, savedSuggestion);
+			}
+		});
+	},
 };
