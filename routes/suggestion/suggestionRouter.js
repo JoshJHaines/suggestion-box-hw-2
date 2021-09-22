41470740f61a964b81bc31a0a48ed8ae09e1ac07
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-22 16:23:38 -0400

    added home route to Router file

---
+++ routes/suggestion/suggestionRouter.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+var suggestionController = require("./controller/suggestionController");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+	res.json({ message: "recipe page" });
+	// animalController.getAllAnimal({}, function (err, foundAnimal) {
+	// 	if (err) {
+	// 		res.status(500).json({
+	// 			message: "Something went wrong!",
+	// 			error: err.message,
+	// 		});
+	// 	} else {
+	// 		res.json({ message: "Success!", foundAnimal });
+	// 	}
+	// });
+});
+
+
+module.exports = router;
