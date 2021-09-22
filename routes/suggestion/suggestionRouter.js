616b87c3209d39baecea529c90f68edc99ee1884
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-22 16:59:18 -0400

    created a search by id route

--- routes/suggestion/suggestionRouter.js
+++ routes/suggestion/suggestionRouter.js
@@ -4,17 +4,30 @@ var suggestionController = require("./controller/suggestionController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-	res.json({ message: "suggestion page" });
-	// animalController.getAllAnimal({}, function (err, foundAnimal) {
-	// 	if (err) {
-	// 		res.status(500).json({
-	// 			message: "Something went wrong!",
-	// 			error: err.message,
-	// 		});
-	// 	} else {
-	// 		res.json({ message: "Success!", foundAnimal });
-	// 	}
-	// });
+	// res.json({ message: "suggestion page" });
+	suggestionController.getAllSuggestion({}, function (err, foundSuggestion) {
+		if (err) {
+			res.status(500).json({
+				message: "Something went wrong!",
+				error: err.message,
+			});
+		} else {
+			res.json({ message: "Success!", foundSuggestion });
+		}
+	});
+});
+
+router.get("/search-by-id/:id", function (req, res, next) {
+	suggestionController.getSuggestionByID(req.params.id, function (err, foundSuggestion) {
+		if (err) {
+			res.status(500).json({
+				message: "Something went wrong!",
+				error: err.message,
+			});
+		} else {
+			res.json({ message: "Success!", foundSuggestion });
+		}
+	});
 });
 
 router.post("/create-suggestion", function (req, res, next) {
