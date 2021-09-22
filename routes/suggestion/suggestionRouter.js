50a2a33ee7b836fc4636c966fde952b83019030e
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-22 16:46:49 -0400

    created a create-suggestion route.

--- routes/suggestion/suggestionRouter.js
+++ routes/suggestion/suggestionRouter.js
@@ -17,5 +17,18 @@ router.get("/", function (req, res, next) {
 	// });
 });
 
+router.post("/create-suggestion", function (req, res, next) {
+	// res.json({ message: "creation page" });
+	suggestionController.createSuggestion(req.body, function (err, savedSuggestion) {
+		if (err) {
+			res.status(500).json({
+				message: "Something went wrong!",
+				error: err.message,
+			});
+		} else {
+			res.json({ message: "success", savedSuggestion });
+		}
+	});
+});
 
 module.exports = router;
